b08c1947cd2f882295a8c22942408289a4f7e9dd
Christopher Renaud Oelerich <christopher.renaud@oeleri.ch>
2023-08-31 07:14:50 -0500

    socketlib test

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,33 @@
-Hooks.once('init', async function() {
+let socket;
+
+Hooks.once("socketlib.ready", () => {
+    socket = socketlib.registerModule("oronder-foundry");
+    socket.register("hello", showHelloMessage);
+    socket.register("add", add);
+});
+
+
+Hooks.once('init', async () => {
 
 });
 
-Hooks.once('ready', async function() {
-
+Hooks.once("ready", async () => {
+    // Let's send a greeting to all other connected users.
+    // Functions can either be called by their given name...
+    socket.executeForEveryone("hello", game.user.name);
+    // ...or by passing in the function that you'd like to call.
+    socket.executeForEveryone(showHelloMessage, game.user.name);
+    // The following function will be executed on a GM client.
+    // The return value will be sent back to us.
+    const result = await socket.executeAsGM("add", 5, 3);
+    console.log(`The GM client calculated: ${result}`);
 });
+
+function showHelloMessage(userName) {
+    console.log(`User ${userName} says hello!`);
+}
+
+function add(a, b) {
+    console.log("The addition is performed on a GM client.");
+    return a + b;
+}
