47183eb75d24e03d2087eec782a48d6a7136393a
Christopher Renaud Oelerich <christopher.renaud@oeleri.ch>
2023-08-31 15:58:11 -0500

    add discord ids to settings

--- src/settings.mjs
+++ src/settings.mjs
@@ -1,7 +1,9 @@
-import {MODULE_NAME} from "./util.mjs";
+import {Logger, MODULE_NAME} from "./util.mjs";
 
 export const registerSettings = () => {
+    Logger.log('registering settings')
     for (const user of game.users) {
+        Logger.log(`${user.name}`)
         game.settings.register(MODULE_NAME, `discord_id.${user.id}`, {
             name: `${user.name}'s Discord User ID`,
             scope: "world",
@@ -9,4 +11,5 @@ export const registerSettings = () => {
             config: true
         })
     }
+    Logger.log('registered settings')
 }
