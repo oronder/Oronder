ebf1c0ec6039eb6d453f65a63211417d55814d45
Christopher Renaud Oelerich <christopher.renaud@oeleri.ch>
2023-09-11 13:51:45 -0500

    use prod api

--- src/constants.mjs
+++ src/constants.mjs
@@ -13,5 +13,5 @@ export const ORONDER_CONFIGURATION_FORM="oronder_options"
 export const VALID_CONFIG = "valid_config"
 export const DISCORD_ID = "discord_id"
 
-export const ORONDER_BASE_URL = 'http://127.0.0.1:65435'
-// export const ORONDER_BASE_URL = 'https://api.oronder.com'
+// export const ORONDER_BASE_URL = 'http://127.0.0.1:65435'
+export const ORONDER_BASE_URL = 'https://api.oronder.com'
