cc014f903bd59fc1ec1d219e781b67de8bd287ad
Christopher Renaud Oelerich <christopher.renaud@oeleri.ch>
2023-12-15 01:54:20 -0600

    update release script

--- release.py
+++ release.py
@@ -11,10 +11,16 @@ import click
               )
 def run_release(release):
     subprocess.run('git pull', shell=True)
-    major, minor, revision = [int(i) for i in subprocess.run(
-        'git describe --abbrev=0 --tags',
-        capture_output=True, shell=True, text=True
-    ).stdout.strip('\n').split('.')]
+    major, minor, revision = max(
+        [
+            [int(v) for v in t.split('.')]
+            for t in
+            subprocess.run('git tag -l', capture_output=True, shell=True, text=True).stdout.strip('\n').split('\n')
+            if t != 'latest'
+        ],
+        key=lambda triple: (triple[0], triple[1], triple[2])
+    )
+
     match release:
         case 'major':
             major += 1
