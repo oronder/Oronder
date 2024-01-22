2c4020fbe2cef8341f426d0bdc834590d90775d9
Christopher Renaud Oelerich <chris@oeleri.ch>
2024-01-22 17:17:58 -0600

    test automated releases

--- .github/workflows/foundry_release.py
+++ .github/workflows/foundry_release.py
@@ -3,10 +3,21 @@ import json
 import os
 from pprint import pprint
 
-print(os.environ.get('MODULE_JSON_PATH'))
 with open(os.environ.get('MODULE_JSON_PATH'), 'r') as file:
     module = json.load(file)
-pprint(module)
+
+body = {
+    'id': module['id'],
+    "dry-run": True,
+    'release': {
+        'version': module['version'],
+        'manifest': module['manifest'],
+        'notes': f"https://github.com/oronder/Oronder/releases/tag/{module['version']}",
+        'compatibility': module['compatibility']
+    }
+}
+pprint(body)
+
 connection = http.client.HTTPSConnection("api.foundryvtt.com")
 connection.request(
     "POST", "/_api/packages/release_version/",
@@ -14,15 +25,6 @@ connection.request(
         'Content-Type': 'application/json',
         'Authorization': os.environ.get('FOUNDRY_PACKAGE_RELEASE_TOKEN')
     },
-    body=json.dumps({
-        'id': module['id'],
-        "dry-run": True,
-        'release': {
-            'version': module['version'],
-            'manifest': module['manifest'],
-            'notes': f"https://github.com/oronder/Oronder/releases/tag/{module['version']}",
-            'compatibility': module['compatibility']
-        }
-    })
+    body=json.dumps(body)
 )
 pprint(json.loads(connection.getresponse().read().decode()))
