0f1791f3b531d1de063611c915d725a35cd8246f
Christopher Renaud Oelerich <chris@oeleri.ch>
2024-01-22 17:07:47 -0600

    test automated releases

---
+++ .github/workflows/foundry_release.py
@@ -0,0 +1,27 @@
+import http.client
+import json
+import os
+from pprint import pprint
+
+with open(os.environ.get('MODULE_JSON_PATH'), 'r') as file:
+    module = json.load(file)
+pprint(module)
+connection = http.client.HTTPSConnection("api.foundryvtt.com")
+connection.request(
+    "POST", "/_api/packages/release_version/",
+    headers={
+        'Content-Type': 'application/json',
+        'Authorization': os.environ.get('FOUNDRY_PACKAGE_RELEASE_TOKEN')
+    },
+    body=json.dumps({
+        'id': module['id'],
+        "dry-run": True,
+        'release': {
+            'version': module['version'],
+            'manifest': module['manifest'],
+            'notes': f"https://github.com/oronder/Oronder/releases/tag/{module['version']}",
+            'compatibility': module['compatibility']
+        }
+    })
+)
+pprint(json.loads(connection.getresponse().read().decode()))
