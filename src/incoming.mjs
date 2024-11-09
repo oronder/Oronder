792ae6cad9034ab99731acb6cb87bb51dfaa46f0
Christopher Renaud Oelerich <chris@oeleri.ch>
2024-11-09 10:33:26 -0500

    Stub out Foundry side /roll support

--- src/incoming.mjs
+++ src/incoming.mjs
@@ -2,11 +2,51 @@ import {get_user, Logger} from './util.mjs'
 import {socket} from './module.mjs'
 
 /**
+ *
+ * @param {D20Roll} roll
+ */
+function roll_to_str(roll) {
+    const formula = roll.terms
+        .map(t => {
+            if ('results' in t) {
+                const f = t.results.some(_ => _.discarded)
+                    ? r => (r.discarded ? `~~${r.result}~~` : `**${r.result}**`)
+                    : r => r.result
+
+                const rolls = t.results.map(f).join(', ')
+                return `${t.expression} (${rolls})`
+            } else {
+                return t.expression
+            }
+        })
+        .join('')
+
+    return `${formula} = \`${roll.total}\``
+}
+
+/**
  @param {Actor} actor
  @param {Object} data
  @param {function(Object)} callback
  */
-async function incoming_roll(actor, data, callback) {}
+async function incoming_roll(actor, data, callback) {
+
+    // if ( ability === "concentration" ) this.actor.rollConcentration({ event, legacy: false });
+    // else if ( isSavingThrow ) this.actor.rollSavingThrow({ ability, event });
+    // else this.actor.rollAbilityCheck({ ability, event });
+    switch (data['type']) {
+        case 'ability':
+            const a = actor.system.abilities[data.stat]
+            break
+        case 'tool':
+            const b = actor.system.tools[data.stat]
+            break
+        case 'skill':
+            const c = actor.system.skills[data.stat]
+            break
+    }
+    callback({})
+}
 
 /**
  @param {Actor} actor
@@ -14,11 +54,11 @@ async function incoming_roll(actor, data, callback) {}
  @param {function(Object)} callback
  */
 async function incoming_attack(actor, data, callback) {
-    Logger.info(`Attack Roll`)
     const item = actor.items.find(i => i.id === data.item_id)
 
     if (item === undefined) {
         Logger.error(game.i18n.localize('oronder.Item-Not-Found'))
+        callback({})
         return
     }
 
@@ -80,8 +120,8 @@ async function incoming_attack(actor, data, callback) {
     }
 
     callback({
-        atk: `${atk.formula} = \`${atk.total}\``,
-        dmg: `${dmg.formula} = \`${dmg.total}\``
+        atk: roll_to_str(atk),
+        dmg: roll_to_str(dmg)
     })
 }
 
@@ -90,6 +130,7 @@ export function set_incoming_hooks() {
         const actor = game.actors.find(a => a.id === data.actor_id)
         if (actor === undefined) {
             Logger.error(game.i18n.localize('oronder.Actor-Not-Found'))
+            callback({})
             return
         }
 
@@ -116,7 +157,6 @@ export function set_incoming_hooks() {
                 await incoming_roll(actor, data, callback)
                 break
             case 'attack':
-                Logger.info(`Attack Roll\n${data}`)
                 await incoming_attack(actor, data, callback)
                 break
         }
